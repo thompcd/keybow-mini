c84a54b6cfbb63dcd913104ca3bf55e56cbcf2ec
Corey Thompson <corey.dwayne.thompson@gmail.com>
2026-02-05 05:57:16 -0600

    Fix hold detection: use os.time() instead of os.clock()

--- layouts/led_colors.lua
+++ layouts/led_colors.lua
@@ -8,7 +8,7 @@ require "keybow"
 
 -- Track press times for hold detection
 local press_time = {}
-local HOLD_THRESHOLD = 3.0  -- seconds
+local HOLD_THRESHOLD = 3  -- seconds
 
 function setup()
     keybow.use_mini()
@@ -22,20 +22,19 @@ function set_all(r, g, b)
     end
 end
 
-function get_time()
-    return os.clock()
-end
-
 function handle_key(index, pressed, r, g, b)
     if pressed then
-        press_time[index] = get_time()
+        press_time[index] = os.time()
     else
-        local held = get_time() - (press_time[index] or 0)
-        if held >= HOLD_THRESHOLD then
-            -- Long hold: turn all off
-            set_all(0, 0, 0)
+        local start = press_time[index]
+        if start then
+            local held = os.time() - start
+            if held >= HOLD_THRESHOLD then
+                set_all(0, 0, 0)
+            else
+                set_all(r, g, b)
+            end
         else
-            -- Short press: set color
             set_all(r, g, b)
         end
         press_time[index] = nil
